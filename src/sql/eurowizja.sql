d44c1fb9a68eb553f150cc7dcc76c3ec4b1fa539
mkocurek7 <58214646+mkocurek7@users.noreply.github.com>
2020-06-10 16:25:57 +0200

    Update eurowizja.sql

--- src/sql/eurowizja.sql
+++ src/sql/eurowizja.sql
@@ -89,13 +89,6 @@ INSERT INTO eliminacje (etap, uczestnik_id, wynik_id, rok_id, daty_id) VALUES
 ('półfinał 1', '117', '116', '3', '3'), ('półfinał 1', '118', '117', '3', '3'), ('półfinał 1', '113', '118', '3', '3'), ('półfinał 1', '107', '8', '3', '3');
 
 
-
-
-
-
-
-
-
 -- półfinał 2
 INSERT INTO eliminacje (etap, uczestnik_id, wynik_id, rok_id, daty_id) VALUES
 ('półfinał 2', '9', '19', '1', '4'), ('półfinał 2', '10', '20', '1', '4'), ('półfinał 2', '11', '21', '1', '4'), ('półfinał 2', '12', '22', '1', '4'), ('półfinał 2', '13', '23', '1', '4'), 
@@ -112,15 +105,27 @@ INSERT INTO eliminacje (etap, uczestnik_id, wynik_id, rok_id, daty_id) VALUES
 
 
 
+-- finał
+INSERT INTO eliminacje (etap, uczestnik_id, wynik_id, rok_id, daty_id) VALUES
+('finał', '16', '37', '1', '7'), ('finał', '17', '38', '1', '7'), ('finał', '18', '14', '1', '7'), ('finał', '19', '39', '1', '7'), ('finał', '20', '40', '1', '7'), 
+('finał', '21', '8', '1', '7'), ('finał', '22', '41', '1', '7'), ('finał', '23', '24', '1', '7'), ('finał', '24', '42', '1', '7'), ('finał', '25', '43', '1', '7'), 
+('finał', '26', '44', '1', '7'), ('finał', '27', '45', '1', '7'), ('finał', '28', '46', '1', '7'), ('finał', '29', '47', '1', '7'), ('finał', '30', '43', '1', '7'), 
+('finał', '31', '48', '1', '7'), ('finał', '32', '49', '1', '7'), ('finał', '33', '50', '1', '7'), ('finał', '34', '51', '1', '7'), ('finał', '35', '52', '1', '7'), 
+('finał', '36', '53', '1', '7'), ('finał', '37', '17', '1', '7'), ('finał', '38', '54', '1', '7'), ('finał', '39', '55', '1', '7'), ('finał', '40', '56', '1', '7'), 
+('finał', '41', '57', '1', '7'), ('finał', '60', '85', '2', '8'), ('finał', '61', '86', '2', '8'), ('finał', '62', '38', '2', '8'), ('finał', '63', '87', '2', '8'), 
+('finał', '64', '88', '2', '8'), ('finał', '65', '89', '2', '8'), ('finał', '66', '90', '2', '8'), ('finał', '67', '37', '2', '8'), ('finał', '68', '91', '2', '8'), 
+('finał', '69', '92', '2', '8'), ('finał', '70', '93', '2', '8'), ('finał', '71', '94', '2', '8'), ('finał', '72', '47', '2', '8'), ('finał', '73', '95', '2', '8'), 
+('finał', '74', '96', '2', '8'), ('finał', '75', '2', '2', '8'), ('finał', '76', '97', '2', '8'), ('finał', '77', '98', '2', '8'), ('finał', '78', '99', '2', '8'), 
+('finał', '79', '100', '2', '8'), ('finał', '80', '39', '2', '8'), ('finał', '81', '101', '2', '8'), ('finał', '82', '102', '2', '8'), ('finał', '83', '103', '2', '8'), 
+('finał', '84', '104', '2', '8'), ('finał', '85', '105', '2', '8'), ('finał', '101', '74', '3', '9'), ('finał', '102', '131', '3', '9'), ('finał', '103', '124', '3', '9'), 
+('finał', '104', '132', '3', '9'), ('finał', '105', '9', '3', '9'), ('finał', '106', '45', '3', '9'), ('finał', '107', '43', '3', '9'), ('finał', '108', '133', '3', '9'), 
+('finał', '109', '42', '3', '9'), ('finał', '110', '134', '3', '9'), ('finał', '111', '135', '3', '9'), ('finał', '112', '136', '3', '9'), ('finał', '113', '137', '3', '9'), 
+('finał', '114', '138', '3', '9'), ('finał', '115', '139', '3', '9'), ('finał', '116', '80', '3', '9'), ('finał', '117', '62', '3', '9'), ('finał', '118', '4', '3', '9'), 
+('finał', '119', '140', '3', '9'), ('finał', '120', '141', '3', '9'), ('finał', '121', '134', '3', '9'), ('finał', '122', '142', '3', '9'), ('finał', '123', '143', '3', '9'), 
+('finał', '124', '144', '3', '9'), ('finał', '125', '145', '3', '9'), ('finał', '126', '146', '3', '9');
 
 
-
-
-
-
-
-
--- tabela zawiera wszystkie wyniki z połfinałó i finałów 
+-- tabela zawiera wszystkie wyniki z połfinałów i finałów 
 INSERT INTO wyniki (punkty) VALUES
 ('227'), ('99'), ('160'), ('76'), ('165'), ('56'), ('92'), ('150'), ('370'), ('115'), 
 ('119'), ('291'), ('60'), ('83'), ('164'), ('152'), ('36'), ('21'), ('98'), ('147'), 
@@ -140,14 +145,11 @@ INSERT INTO wyniki (punkty) VALUES
 
 
 
-
-
-
-
-
-
-
-
+-- daty wszystkich półfinałów1, półfinałów2 i finałów
+INSERT INTO daty (data_, rok_id) VALUES 
+('9 maj', '1'), ('8 maj', '2'), ('14 maj','3'), 
+('11 maj','1'), ('10 maj','2'), ('16 maj','3'),
+('13 maj','1'), ('12 maj','2'), ('18 maj','3');
 
 
 -- wszystkie kraje biorące udział w eurowizji
@@ -163,4 +165,79 @@ INSERT INTO kraje (kraj, kontynent, stolica) VALUES
 ('Irlandia', 'Europa', 'Dublin'), ('Cypr', 'Europa', 'Nikozja'), ('Włochy', 'Europa', 'Rzym');
 
 
+-- uczestnicy rok 2017 
+INSERT INTO uczestnicy (uczestnik, kraj_id, rok_id, piosenka_id) VALUES
+('Tamara Gaczecziladze', '13', '1', '1'), ('Lindita', '29', '1', '2'), ('Slavko Kalezić', '17', '1', '3'), ('Norma John', '34', '1', '4'), ('Svala', '2', '1', '5'), 
+('Martina Bárta', '31', '1', '6'),  ('Omar Naber', '20', '1', '7'), ('Triana Park', '16', '1', '8'), ('Tijana Bogićević', '27', '1', '9'), ("Jana Burčeska", '5', '1', '10'), 
+('Claudia Faniello', '15', '1', '11'), ('Brendan Murray', '41', '1', '12'), ('Valentina Monetta i Jimmie Wilson', '11', '1', '13'),  ('Timebelle', '9', '1', '14'), ('Koit Toome and Laura', '23', '1', '15'),  
+('IMRI', '39', '1', '16'), ("Kasia Moś", "14", '1', '17'), ('Naviband', "4", '1', '18'), ('Nathan Trent', '22', '1', '19'), ('Artsvik', '8', '1', '20'), 
+('OG3NE', '40', '1', '21'), ('Sunstroke Project', '36', '1', '22'),  ('Joci Pápai', '38', '1', '23'), ('Francesco Gabbani', '43', '1', '24'), ('Anja', '32', '1', '25'), 
+('Salvador Sobral', '25', '1', '26'),  ('Dihaj', '1', '1', '27'), ('Jacques Houdek', '6', '1', '28'), ('Isaiah', '33', '1', '29'),('Demy', '7', '1', '30'), 
+('Manel Navarro', '19', '1', '31'),  ('JOWST', '24', '1', '32'), ('Lucie Jones', '26', '1', '33'), ('Hovig', '42', '1', '34'), ('Ilinca ft. Alex Florea', '10', '1', '35'),  
+('Levina', '28', '1', '36'), ('O.Torvald', '18', '1', '37'), ('Blanche', '3', '1', '38'), ('Robin Bengtsson', '37', '1', '39'),  ('Kristian Kostov', '35', '1', '40'), 
+('Alma', '30', '1', '41'), ('Fusedmarc', '21', '1', '42');
 
+
+
+
+
+
+
+
+/* uczestnicy rok 2019 */
+INSERT INTO uczestnicy (uczestnik, kraj_id, rok_id, piosenka_id) VALUES
+('D mol', '17', '3', '86'), ('Darude feat. Sebastian Rejman', '34', '3', '87'), ('Tulia', '14', '3', '88'), ('Joci Pápai', '38', '3', '89'), ('Eliot', '3', '3', '90'), 
+('Oto Nemsadze', '13', '3', '91'), ('Conan Osiris', '25', '3', '92'), ('Srbuk', '8', '3', '93'), ('Sarah McTernan', '41', '3', '94'), ('Anna Odobescu', '36', '3', '95'), 
+('Carousel', '16', '3', '96'), ('Ester Peony', '10', '3', '97'), ('PÆNDA', '22', '3', '98'), ('Roko', '6', '3', '99'), ('Jurij Veklenko', '21', '3', '100'), 
+('Michela', '15', '3', '101'), ('Jonida Maliqi', '29', '3', '102'), ('Lake Malawi', '31', '3', '103'), ('S!sters', '28', '3', '104'), ('Sergey Lazarev', '12', '3', '105'), 
+('Leonora', '32', '3', '106'), ('Serhat', '11', '3', '107'), ('Tamara Todevska', '5', '3', '108'), ('John Lundvik', '37', '3', '109'), ('Zala Kralj & Gašper Šantl', '20', '3', '110'), 
+('Tamta', '42', '3', '111'), ('Duncan Laurence', '40', '3', '112'), ('Katerine Duska', '7', '3', '113'), ('Kobi Marimi', '39', '3', '114'), ('KEiiNO', '24', '3', '115'), 
+('Michael Rice', '26', '3', '116'), ('Hatari', '2', '3', '117'), ('Victor Crone', '23', '3', '118'), ('ZENA', '4', '3', '119'), ('Chingiz', '1', '3', '120'), 
+('Bilal Hassani', '30', '3', '121'), ('Mahmood', '43', '3', '122'), ('Nevena Božović', '27', '3', '123'), ('Luca Hänni', '9', '3', '124'), ('Kate Miller-Heidke', '33', '3', '125'), 
+('Miki', '19', '3', '126');
+
+
+
+
+
+
+
+
+/* piosenki 2018 */
+INSERT INTO piosenki (tytul, jezyk, uczestnik_id, rok_id) VALUES
+('X My Heart', 'angielski', '43', '2'), ("Our Choice", 'angielski', '44', '2'), ("A Matter of Time", 'angielski', '45', '2'), ("Forever", 'angielski', '46', '2'), ("Lost and Found", 'angielski', '47', '2'), 
+("Crazy", 'angielski', '48', '2'), ("Oniro mou", 'grecki', '49', '2'), ("Qami", 'armeński', '50', '2'), ("Stones", 'angielski', '51', '2'), ("Goodbye", 'angielski', '52', '2'), 
+("Who We Are", 'angielski', '53', '2'), ("I Won t Break", 'angielski', '54', '2'), ("For You", 'gruziński', '55', '2'), ("Light Me Up", 'angielski', '56', '2'), ("Taboo", 'angielski', '57', '2'), 
+("Funny Girl", 'angielski', '58', '2'), ("Inje", 'czarnogórski', '59', '2'), ("Under the Ladder", 'angielski', '60', '2'), ("Tu canción", 'hiszpański', '61', '2'), ('Hvala, ne!', 'słoweński', '62', '2'), 
+("When We are Old", 'angielski', '63', '2'), ("Nobody but You", 'angielski', '64', '2'), ("La forza", 'włoski', '65', '2'), ("That s How You Write a Song", 'angielski', '66', '2'), ("O jardim",  'portugalski', '67', '2'), 
+("Storm", 'angielski', '68', '2'), ("Nova deca",  'serbski', '69', '2'), ("You Let Me Walk Alone", 'angielski', '70', '2'), ("Mall", 'albański', '71', '2'), ("Mercy", 'francuski', '72', '2'), 
+("Lie to Me", 'angielski', '73', '2'), ("Higher Ground", 'angielski', '74', '2'), ("We Got Love", 'angielski', '75', '2'), ("Monsters", 'angielski', '76', '2'), ("Bones", 'angielski', '77', '2'), 
+("My Lucky Day", 'angielski', '78', '2'), ("Dance You Off", 'angielski', '79', '2'), ('Viszlát nyár', 'węgierski', '80', '2'), ("Toy", 'angielski', '81', '2'), ("Outlaw in Em", 'angielski', '82', '2'), 
+('Together', 'angielski', '83', '2'), ("Fuego", 'angielski', '84', '2'), ("Non mi avete fatto niente", 'włoski', '85', '2');
+
+
+
+
+
+
+
+
+
+UPDATE roczniki SET uczestnik_id = 26 WHERE rok_id = 1;
+UPDATE roczniki SET piosenka_id = 26 WHERE rok_id = 1;
+
+UPDATE roczniki SET uczestnik_id = 81 WHERE rok_id = 2;
+UPDATE roczniki SET piosenka_id = 81 WHERE rok_id = 2;
+
+UPDATE roczniki SET uczestnik_id = 112 WHERE rok_id = 3;
+UPDATE roczniki SET piosenka_id = 112 WHERE rok_id = 3;
+
+
+
+
+
+
+
+CREATE USER 'eurowizja'@'localhost' IDENTIFIED BY 'eurowizja';
+GRANT ALL PRIVILEGES ON eurowizja.* TO 'eurowizja'@'localhost';
+FLUSH PRIVILEGES;
